e0d8a45d064442f6de061581660d216d0fdd41b7
Cemothy <Robin.Stockinger@gmx.de>
2023-04-18 17:37:07 +0200

    MapColouring

---
+++ examples/MapColouring/MapColouring.py
@@ -0,0 +1,47 @@
+
+from constraint import Problem, AllDifferentConstraint
+
+# Check http://www.csc.fi/oppaat/f95/python/talot.py
+
+
+def solve():
+    problem = Problem()
+    problem.addVariables(["BW","BAY","SARL","RHPF","HES","THU","SAX","SAA","NRW","NS","BER","BRAND","BREM","HAM","SLH","MVP"], range(1, 4))
+
+#CEM SARL -NRW
+#STEVE NS -MVP
+
+    #BW
+    problem.addConstraint(
+       lambda b1,b2,b3,b4: b1 not in [b2,b3,b4], ("BW","BAY","HES","RHPF")
+    )
+
+    #BAY
+    problem.addConstraint(
+        lambda b1,b2,b3,b4,b5: b1 not in[b2,b3,b4,b5], ("BAY","BW","HES","THU","SAX")
+    )
+
+
+    solutions = problem.getSolutions()
+    return solutions
+
+
+def showSolution(solution):
+    for i in range(1,5):
+        print("Room %d:" % i)
+        print("")
+
+
+
+def main():
+    solutions = solve()
+    print("Found %d solution(s)!" % len(solutions))
+    print("")
+    for solution in solutions:
+        print(solution)
+
+
+
+
+if __name__ == "__main__":
+    main()
