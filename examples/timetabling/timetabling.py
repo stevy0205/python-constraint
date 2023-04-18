7f407329976e2a9f746b823ea3fb2178c3aee394
Cemothy <Robin.Stockinger@gmx.de>
2023-04-18 13:13:33 +0200

    Hint 1-6

--- examples/timetabling/timetabling.py
+++ examples/timetabling/timetabling.py
@@ -35,6 +35,42 @@ def solve():
             ("name%d" % i, "room%d" % i ),
         )
 
+        #Hint 2
+        problem.addConstraint(
+            lambda name, subject: name != "Maier" or subject == "deutsch",
+            ("name%d" %i, "room%d" % i),
+        )
+
+        #Hint 3
+        if i > i < 5:
+            problem.addConstraint(
+                lambda namea, nameb, rooma, roomb:
+                    (namea != "Schmid" or nameb != "Müller") and
+                    (namea != "Müller" or nameb != "SChmid") and
+                    abs(rooma - roomb) > 1,
+                ("room%d" % (i-1), "room%d" % i, "name%d" % (i-1), "name%d" % i)
+            )
+
+        #Hint 4
+        problem.addConstraint(
+            lambda name, subject: name != "Huber" or subject == "mathe",
+            ("name%d" % i, "subject%d" % i),
+        )
+
+        #Hint 5
+        problem.addConstraint(
+            lambda subject, room: subject != "physik" or room == "4",
+            ("subject%d" % i, "room%d" % i),
+        )
+
+        #Hint6
+        problem.addConstraint(
+            lambda subjecta, subjectb, room:
+                (subjecta != "deutsch" or room != "1") and
+                (subjectb != "englisch" or room != "1")
+            ("subject%d" % i, "subject%d" % i, "room%d" % i),
+        )
+
     solutions = problem.getSolutions()
     return solutions
 
